ae45e892e7de7ec2fd6ecd5ed899155aa94d24b7
erik-cf <erikcf95@gmail.com>
2019-04-08 10:54:07 +0200

    Fin

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
  <section id="missatges">
 <?php
 
-
 $consulta = "SELECT * FROM missatges ORDER BY id DESC LIMIT 10";
 $resultat = mysqli_query($connexio, $consulta);
 
@@ -25,37 +24,39 @@ while ($fila = mysqli_fetch_assoc($resultat)) {
 
 mysqli_free_result($resultat);
 mysqli_close($connexio);
+
 ?>
  </section>
  <section id="formulari">
- <form method="post" action="insertar.php">
- <table border=0>
- <tr>
- <td><p>Usuari: </p></td><td><p>
- <?php if(isset($_GET['user'])){
- 	print("<input type='text' name='usuari' value=".$_GET['user']." />");
- }else{
- 	print("<input type='text' name='usuari' />");
- }
- ?>
- </p></td></tr>
- <tr>
- <td><p>Missatge: </p></td><td><p>
- <?php
- if(isset($_GET['msg'])){
- 	print("<input type='text' name='missatge' value=".$_GET['msg']." />");
- }else{
- 	print("<input type='text' name='missatge' />");
- }
- ?>
- </p></td>
- </tr>
- <tr>
- <td colspan="2">
-<p><input type="submit" value="Enviar missatge" /></p></td>
-</tr>
-</table>
- </form>
+  <form method="post" action="insertar.php">
+   <table border=0>
+    <tr>
+     <td><p>Usuari: </p></td><td><p>
+     <?php if(isset($_GET['user'])){
+ 	    print("<input type='text' name='usuari' value=".$_GET['user']." />");
+     }else{
+ 	    print("<input type='text' name='usuari' />");
+     }
+     ?>
+     </p></td></tr>
+    <tr>
+     <td><p>Missatge: </p></td><td><p>
+     <?php
+      if(isset($_GET['msg'])){
+ 	    print("<input type='text' name='missatge' value=".$_GET['msg']." />");
+      }else{
+ 	    print("<input type='text' name='missatge' />");
+      }
+     ?>
+     </p></td>
+    </tr>
+    <tr>
+     <td colspan="2">
+      <p><input type="submit" value="Enviar missatge" /></p>
+     </td>
+    </tr>
+   </table>
+  </form>
  </section>
  <?php
  	if(isset($_GET['newmsg'])){
