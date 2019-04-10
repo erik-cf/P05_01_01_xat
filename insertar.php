d6b82177aead98cb6fd4f41144e975a4c8812200
erik-cf <erikcf95@gmail.com>
2019-04-10 09:36:16 +0200

    Final pràctica xat

--- insertar.php
+++ insertar.php
@@ -2,23 +2,25 @@
 	include('connexioBD.php');
 	if(!isset($_POST['usuari']) || empty($_POST['usuari'])){
 		if(!empty($_POST['missatge'])){
-			header("Location: index.php?error=true&nouser=true&msg=".$_POST['missatge']);
+			header("Location: index.php?error&nouser&msg=".$_POST['missatge']);
 			exit();
 		}else{
-			header("Location: index.php?error=true&nouser=true&nomsg=true");
+			header("Location: index.php?error&nouser&nomsg");
 			exit();	
 		}
 	}elseif(!isset($_POST['missatge']) || empty($_POST['missatge'])){
-		header("Location: index.php?error=true&nomsg=true&user=".$_POST['usuari']);
+		header("Location: index.php?error&nomsg&user=".$_POST['usuari']);
 		exit();
 	}else{
 		$missatge = mysqli_real_escape_string($connexio, $_POST['missatge']);
 		$usuari = mysqli_real_escape_string($connexio, $_POST['usuari']);
 		$insert = "INSERT INTO missatges(usuari, text, hora) VALUES ('".htmlspecialchars($usuari)."', '".htmlspecialchars($missatge)."', '".date("H:i:s")."')";
 		if ($connexio->query($insert) === TRUE) {
-    		header("Location: index.php?newmsg=true");
+    		header("Location: index.php?newmsg");
+    		exit();
 		} else {
-    		header("Location: index.php?error=true&noinsert=".$connexio->error);
+    		header("Location: index.php?error&noinsert=".$connexio->error);
+    		exit();
 		}
 	}
 ?>
